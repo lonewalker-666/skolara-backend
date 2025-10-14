2b77e352639d5a376cee08a8daad2d304e8b1530
= <=>
2025-10-14 22:45:16 +0530

    upload document completed

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -6,5 +6,14 @@ export const updateUserProfileSchema = z.object({
   email: z.string().email(),
   mobile: z.string().min(10).max(10),
   gender: z.enum(["Male", "Female", "Others"]).nullable(),
-  dob: z.date().nullable(),
+    // keep as string with YYYY-MM-DD
+  dob: z.union([
+    z
+      .string()
+      .regex(/^\d{4}-\d{2}-\d{2}$/, "Use YYYY-MM-DD")
+      .transform((s) => new Date(`${s}T00:00:00.000Z`)),
+    z.null(),
+  ])
+    .nullable()
+    .default(null),
 });
